b6d3933e88ed695d3a30ef80924c8a19f34a1175
Antonio Abella <antonioabella@gmail.com>
2019-01-08 11:00:02 -0400

    feat: count/slice implementation of Enumerable protocol

--- lib/lazy_map.ex
+++ lib/lazy_map.ex
@@ -3,4 +3,36 @@ defmodule LazyMap do
   Lazy implementation of Access behaviour.
   Substitutes `map` when lazy value access is needed.
   """
+  defstruct map: %{}
+
+  @doc """
+  Creates a new LazyMap
+  """
+  def new, do: %LazyMap{}
+  def new(map), do: %LazyMap{map: map}
+
+  def size(%LazyMap{map: map}) do
+    map_size(map)
+  end
+
+  def to_list(%LazyMap{map: map}) do
+    Map.keys(map)
+  end
+
+  defimpl Enumerable do
+    def count(lazy_map) do
+      {:ok, LazyMap.size(lazy_map)}
+    end
+
+    # def member?(lazy_map, val) do
+    # end
+
+    def slice(lazy_map) do
+      {:ok, LazyMap.size(lazy_map), &Enumerable.List.slice(LazyMap.to_list(lazy_map), &1, &2)}
+    end
+
+    # def reduce(lazy_map, acc, fun) do
+    #   lazy_map
+    # end
+  end
 end
--- test/lazy_map_test.exs
+++ test/lazy_map_test.exs
@@ -1,3 +1,8 @@
 defmodule LazyMapTest do
   use ExUnit.Case
+
+  test "create an empty map" do
+    lm = LazyMap.new()
+    assert Enum.empty?(lm)
+  end
 end
