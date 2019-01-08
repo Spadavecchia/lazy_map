88084d332ee43d4746fa988f6448b47dd1a66c15
Antonio Abella <antonioabella@gmail.com>
2019-01-08 11:04:44 -0400

    test: Enumerable.count implementation

--- test/lazy_map_test.exs
+++ test/lazy_map_test.exs
@@ -5,4 +5,9 @@ defmodule LazyMapTest do
     lm = LazyMap.new()
     assert Enum.empty?(lm)
   end
+
+  test "create a lazy map with two elements" do
+    lm = LazyMap.new(%{first: "hello", second: "world"})
+    assert 2 == Enum.count(lm)
+  end
 end
