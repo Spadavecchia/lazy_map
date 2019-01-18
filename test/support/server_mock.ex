69fa22cd583b26b69c2c4a276c26ac59f1fd0621
Antonio Abella <antonioabella@gmail.com>
2019-01-17 21:18:55 -0400

    style: apply Elixir formatter

--- test/support/server_mock.ex
+++ test/support/server_mock.ex
@@ -13,7 +13,7 @@ defmodule Test.Support.ServerMock do
     GenServer.call(__MODULE__, {:providers, product_name})
   end
 
-  def init(_), do: {:ok, DBMock.products}
+  def init(_), do: {:ok, DBMock.products()}
 
   def handle_call({:providers, product_name}, _from, products) do
     providers = products[product_name]
